b99b843abe22d6e2334d5cae04b55f2585592cc2
lunar-seal <lunar-seal@ff15.eu>
2026-08-21 00:45:06 +0200

    build webkitgtk with webrtc

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "WebKitGTK 4.1 with LibWebRTC from an upstream GTK nightly";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    webkit = {
+      url = "git+https://github.com/WebKit/WebKit.git?ref=refs/heads/main&shallow=1";
+      flake = false;
+    };
+  };
+
+  outputs = { nixpkgs, webkit, ... }:
+    let
+      overlay = final: prev: {
+        webkitgtk_4_1 = (prev.webkitgtk_4_1.override {
+          enableExperimental = true;
+        }).overrideAttrs (old: {
+          pname = "webkitgtk-webrtc";
+          version = "0-unstable-${builtins.substring 0 8 webkit.lastModifiedDate}-${webkit.shortRev}";
+          src = webkit;
+
+          patches = [ (builtins.head old.patches) ];
+          cmakeFlags = old.cmakeFlags ++ [
+            "-DENABLE_MEDIA_STREAM=ON"
+            "-DENABLE_WEB_RTC=ON"
+          ];
+          postPatch = old.postPatch + ''
+            test -d Source/ThirdParty/libwebrtc/Source
+          '';
+
+          meta = old.meta // {
+            description = "WebKitGTK 4.1 with LibWebRTC enabled";
+          };
+        });
+      };
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [ overlay ];
+      };
+    in {
+      overlays.default = overlay;
+      packages.x86_64-linux.default = pkgs.webkitgtk_4_1;
+    };
+}
